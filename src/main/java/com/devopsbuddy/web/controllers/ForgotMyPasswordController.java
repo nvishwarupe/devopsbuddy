517628864d4c491bb285d4660ba259064134b490
Neha Vishwarupe <neha.vishwarupe@gmail.com>
2017-12-16 00:00:26 -0800

    Implemented the first part of forgot my password journey

---
+++ src/main/java/com/devopsbuddy/web/controllers/ForgotMyPasswordController.java
@@ -0,0 +1,116 @@
+package com.devopsbuddy.web.controllers;
+
+import com.devopsbuddy.backend.persistence.domain.backend.PasswordResetToken;
+import com.devopsbuddy.backend.persistence.domain.backend.User;
+import com.devopsbuddy.backend.service.EmailService;
+import com.devopsbuddy.backend.service.PasswordResetTokenService;
+import com.devopsbuddy.backend.service.UserService;
+import com.devopsbuddy.utils.UsersUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+import com.devopsbuddy.web.i18n.I18NService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.Locale;
+
+/**
+ * Created by nvishwarupe
+ */
+@Controller
+public class ForgotMyPasswordController {
+
+    /**
+     * The application logger
+     */
+    private static final Logger LOG = LoggerFactory.getLogger(ForgotMyPasswordController.class);
+
+    public static final String EMAIL_ADDRESS_VIEW_NAME = "forgotmypassword/emailForm";
+
+    public static final String FORGOT_PASSWORD_URL_MAPPING = "/forgotmypassword";
+
+    public static final String MAIL_SENT_KEY = "mailSent";
+
+    public static final String CHANGE_PASSWORD_PATH = "/changeuserpassword";
+
+    public static final String EMAIL_MESSAGE_TEXT_PROPERTY_NAME = "forgotmypassword.email.text";
+
+    public static final String CHANGE_PASSWORD_VIEW_NAME = "forgotmypassword/changePassword";
+
+    private static final String PASSWORD_RESET_ATTRIBUTE_NAME = "passwordReset";
+
+    private static final String MESSAGE_ATTRIBUTE_NAME = "message";
+
+    @Autowired
+    private I18NService i18NService;
+
+
+    @Autowired
+    private EmailService emailService;
+
+    @Value("${webmaster.email}")
+    private String webMasterEmail;
+
+    @Autowired
+    private PasswordResetTokenService passwordResetTokenService;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = FORGOT_PASSWORD_URL_MAPPING, method = RequestMethod.GET)
+    public String forgotPasswordGet() {
+        return EMAIL_ADDRESS_VIEW_NAME;
+    }
+
+
+    @RequestMapping(value = FORGOT_PASSWORD_URL_MAPPING, method = RequestMethod.POST)
+    public String forgotPasswordPost(HttpServletRequest request,
+                                     @RequestParam("email") String email,
+                                     ModelMap model) {
+
+        PasswordResetToken passwordResetToken = passwordResetTokenService.createPasswordResetTokenForEmail(email);
+
+        if (null == passwordResetToken) {
+            LOG.warn("Couldn't find a password reset token for email {}", email);
+        } else {
+
+            User user = passwordResetToken.getUser();
+            String token = passwordResetToken.getToken();
+
+            String resetPasswordUrl = UsersUtils.createPasswordResetUrl(request, user.getId(), token);
+            LOG.debug("Reset Password URL {}", resetPasswordUrl);
+
+            String emailText = i18NService.getMessage(EMAIL_MESSAGE_TEXT_PROPERTY_NAME, request.getLocale());
+
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setTo(user.getEmail());
+            mailMessage.setSubject("[Devopsbuddy]: How to Reset Your Password");
+            mailMessage.setText(emailText + "\r\n" + resetPasswordUrl);
+            mailMessage.setFrom(webMasterEmail);
+
+            emailService.sendGenericEmailMessage(mailMessage);
+        }
+
+        model.addAttribute(MAIL_SENT_KEY, "true");
+
+        return EMAIL_ADDRESS_VIEW_NAME;
+    }
+
+
+
+}
